e5f32eed17e9cb8b822c7f4cdab04ed7b678dbf4
kltachi <58478335+kltachi@users.noreply.github.com>
2019-12-07 04:27:45 +0200

    Add files via upload
    final version

---
+++ AvlTree.h
@@ -0,0 +1,36 @@
+#ifndef AVL_TREE_H_
+#define AVL_TREE_H_
+
+
+/* AVL node */
+template <class T>
+class AVLNode {
+public:
+	T key;
+	AVLNode *left, *right;
+	int depth;
+
+	AVLNode(T k) : key(k), depth(1), left(NULL), right(NULL) {}
+	~AVLNode() {}
+};
+
+
+
+/* AVL tree */
+template <class T>
+class AVLTree {
+public:
+	AVLNode<T>* root;
+
+	AVLTree() : root(NULL) {}
+	~AVLTree();
+
+	void InsertKey(T key);
+	void DeleteKey(T key);
+
+	void PrintInOrder();//for debugging purposes
+};
+
+
+
+#endif
